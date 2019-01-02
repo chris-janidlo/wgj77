37907604fbae26ee27a338b59cc9bd93ac08c657
crassSandwich <christopherjanidlo@gmail.com>
2019-01-02 15:14:18 -0500

    Pause Menu

---
+++ Assets/Scripts/Menus/PauseActivator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseActivator : MonoBehaviour
+{
+    public PausedWhenActive PauseMenu;
+
+    void Update ()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            PauseMenu.enabled = !PauseMenu.enabled;
+            PauseMenu.gameObject.SetActive(!PauseMenu.gameObject.activeSelf);
+        }
+    }
+}
---
+++ Assets/Scripts/Menus/PauseButton.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Text), typeof(Button))]
+public class PauseButton : MonoBehaviour
+{
+    public GameObject PauseMenu;
+    public string DisplayText;
+    public PauseButtonType Type;
+
+    Text text;
+    Button button;
+
+    void Start()
+    {
+        text = GetComponent<Text>();
+        button = GetComponent<Button>();
+
+        button.onClick.AddListener(onClick);
+    }
+
+    void OnSelect ()
+    {
+        text.text = "> " + DisplayText;
+    }
+
+    void OnDeselect ()
+    {
+        text.text = DisplayText;
+    }
+
+    void onClick ()
+    {
+        switch (Type)
+        {
+            case PauseButtonType.Restart:
+                SceneManager.LoadScene("Menu");
+                break;
+
+            case PauseButtonType.Resume:
+                Destroy(PauseMenu);
+                break;
+        }
+    }
+}
+
+public enum PauseButtonType { Resume, Restart }
